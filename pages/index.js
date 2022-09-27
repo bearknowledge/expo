3a40c916583fbd31c3da370b88684a6b20bfff73
Zach Miller <home@mac-2.local>
2022-09-27 14:49:32 -0400

    mobile

--- pages/index.js
+++ pages/index.js
@@ -14,10 +14,13 @@ export default function Home() {
         <title>Zach Miller</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🧪</text></svg>"/>
+ 
       </Head>
+    
       <main className={styles.main}>
-        
-        <Spline scene="https://prod.spline.design/4zK7MtYE7amWhOH7/scene.splinecode"/>
+  
+        <Spline className={styles.desktop} scene="https://prod.spline.design/4zK7MtYE7amWhOH7/scene.splinecode"/>
+       
       </main>
     </>
   )
